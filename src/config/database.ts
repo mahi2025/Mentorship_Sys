fca0f64f069061f3912a4b84e884b7f3a72f1bd6
mahi2025 <mahletmasresha08@gmail.com>
2026-07-02 21:04:32 +0300

    chore: kysely configration added

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,7 +5,7 @@ export async function connectDB() {
   try {
     await sql`SELECT 1`.execute(db);
 
-    console.log("PostgreSQL connected");
+    console.log("Database connected");
   } catch (error) {
     console.error("Database connection failed", error);
     process.exit(1);
