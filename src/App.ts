42290680ce19b980e28feaccc1b7686b99cecdd4
mahi2025 <mahletmasresha08@gmail.com>
2026-07-03 00:14:15 +0300

    feat: auth routes added

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import userRoutes from "./modules/user/user.routes";
+import authRoutes from "./modules/auth/auth.routes";
+
+
 
 
 const app = express();
@@ -7,5 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", userRoutes);
+app.use("/auth", authRoutes);
+
 
 export default app;
