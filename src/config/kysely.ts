9f74c466ba4d9d8ae349b611332822255782368c
mahi2025 <mahletmasresha08@gmail.com>
2026-07-09 21:20:09 +0300

    fix: manage betterauth and kysely connection

--- src/config/kysely.ts
+++
@@ -1,9 +0,0 @@
-import { Kysely, PostgresDialect } from "kysely";
-import { pool } from "./db_pool";
-import type { DB } from "../database/types";
-
-export const db = new Kysely<DB>({
-  dialect: new PostgresDialect({
-    pool,
-  }),
-});
