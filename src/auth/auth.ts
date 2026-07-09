138453e82af99b6f041b6e9d3aa2ec4c001d060c
mahi2025 <mahletmasresha08@gmail.com>
2026-07-09 21:22:28 +0300

    fix: updated auth config

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,43 +1,24 @@
-import { betterAuth, facebook } from "better-auth";
 import { pool } from "../config/database";
-import { PostgresDialect } from 'kysely';
+import { PostgresDialect } from "kysely";
+import { betterAuth } from "better-auth";
 
 export const auth = betterAuth({
-  secret: process.env.BETTER_AUTH_SECRET!,
-  baseURL: process.env.BETTER_AUTH_URL!,
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL,
   database: {
     dialect: new PostgresDialect({ pool }),
     type: "postgres",
-    schema: "auth",
+    schema: "public",
   },
   trustedOrigins: ["http://localhost:5000"],
-
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
   },
-
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
   },
-    /*
-    hooks: {
-      afterCreate: async ({ user, db }) => {
-        await db
-          .insertInto("profile")
-          .values({
-            user_id: user.id,
-            headline: "",
-            bio: "",
-            location: "",
-          })
-          .execute();
-      },
-    },
-    */
 });
-
-
