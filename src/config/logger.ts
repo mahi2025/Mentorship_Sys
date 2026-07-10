8f31ea57738c35e823c82e0b1d9348391a1855b3
mahi2025 <mahletmasresha08@gmail.com>
2026-07-10 16:21:17 +0300

    config(logger): logger configured

---
+++ src/config/logger.ts
@@ -0,0 +1,24 @@
+import winston from "winston";
+import { env } from "./env";
+
+const { combine, timestamp, printf, colorize, json, errors } = winston.format;
+
+const devFormat = combine(
+  colorize(),
+  timestamp({ format: "HH:mm:ss" }),
+  errors({ stack: true }),
+  printf(({ level, message, timestamp, stack, ...meta }) => {
+    const metaStr = Object.keys(meta).length ? JSON.stringify(meta) : "";
+    return `${timestamp} ${level}: ${stack || message} ${metaStr}`;
+  }),
+);
+
+const prodFormat = combine(timestamp(), errors({ stack: true }), json());
+
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === "production" ? "http" : "debug",
+  format: process.env.NODE_ENV === "production" ? prodFormat : devFormat,
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
