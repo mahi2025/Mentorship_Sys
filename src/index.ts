0e9eb143aa1b2be3b76303a8bc301440cecf335f
mahi2025 <mahletmasresha08@gmail.com>
2026-07-10 14:05:26 +0300

    feat(start): redis clinet connected

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import app from "./App";
 import { env} from "./config/env";
 import { connectDB } from "./config/database";
-
+import redisClient from "./config/redis";
 
 const start = async () => {
+  
   await connectDB();
+  
+    if (!redisClient.isOpen) {
+      await redisClient.connect();
+    }
+
+  const { default: app } = await import("./App.js");
 
   app.listen(env.PORT, () => {
     console.log(`running on port ${env.PORT}`);
