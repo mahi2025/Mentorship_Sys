f879b417b866100dc0bac6a45df5c2e2b261577a
mahi2025 <mahletmasresha08@gmail.com>
2026-07-10 16:20:19 +0300

    chore(auth): logger added but not need in this feature

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,26 +1,25 @@
+import logger from "../../config/logger";
 import type { Request, Response, NextFunction } from "express";
 import { AuthService } from "./auth.service";
 import { signUpSchema } from "./auth.validation";
 import { ApiResponse } from "../../shared/responses/ApiResponse";
 
 const authService = new AuthService();
-export async function signUp(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
+
+export async function signUp(req: Request, res: Response, next: NextFunction) {
   try {
     const input = signUpSchema.parse(req.body);
     const name = input.name ?? input.email.split("@")[0];
-    
+
     const result = await authService.signUp({
       email: input.email,
       password: input.password,
       name,
     });
 
+    logger.info("user signed up", { userId: result});
     return ApiResponse.created(res, result, "User created successfully");
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    next(err); 
   }
 }
