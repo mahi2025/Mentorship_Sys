73d3b44cabd168ea2f935630206cf053f2ac8392
mahi2025 <mahletmasresha08@gmail.com>
2026-07-10 14:07:36 +0300

    feat(auth): ratelimiter added

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { signUp } from "./auth.controller";
+import { createAuthLimiter } from "../../middleware/rateLimit.middleware";
 
 const router = Router();
+const authlimiter = createAuthLimiter();
 
-router.post("/sign-up", signUp);
+router.post("/sign-up", authlimiter, signUp);
+
+//router.post("/login", authenticate, authLimiter, login);
+
 /*
 POST   /auth/sign-in
 POST   /auth/sign-out
